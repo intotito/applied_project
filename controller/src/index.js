668167e3e657c37a488bdc9261b0bd48d866e36c
intotito <intotito@gmail.com>
2023-12-07 18:40:42 +0000

    Controller Application Started
    Trials started for the Controller application
    Docker file added for containerization

--- controller/src/index.js
+++ controller/src/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const router = express.Router();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-router.get('/', (req, res) => {
-    res.status(200).render('index');
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
