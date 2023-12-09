4df2ffcb6872aed18d2498002c6590e6b4378225
intotito <intotito@gmail.com>
2023-12-09 02:55:49 +0000

    Reading data from firebase
    Concurrency issues still persists for handling promises

--- controller/src/index.js
+++ controller/src/index.js
@@ -21,7 +21,7 @@ app.listen(port, () => {
  // console.log('Read the document', doc);
 
 const db = firestore.firestore;
-
+/*
 
 const snapshot = db.collection('users').get();
 snapshot.then(data => data.forEach((doc) => {
@@ -29,4 +29,79 @@ snapshot.then(data => data.forEach((doc) => {
 })
 ).catch(error => console.log(error));
 
+*/
 
+const returnable = [];
+
+async function getAllUsersId(){
+    const snapshot = db.collection('users').get();
+    snapshot.then(querySnapshot => {
+            querySnapshot.forEach((result) => {
+                const valuable = {};
+                const user = result.data().handle;
+                valuable._user = user;
+                db.collection('users')
+                .doc(result.id)
+                .collection('tests')
+                .get()
+                .then(querySnapshot1 => {
+                    querySnapshot1.forEach((result1) => {
+                        const testData = result1.data();
+                        if(testData.timestamp){
+                            valuable._date = testData.timestamp?.toDate().toDateString();
+                        } else {
+                            valuable._date = null;
+                        }
+                        const date = testData.timestamp?.toDate().toDateString();
+                        if(testData.audio){
+                            const au_avgResp = testData.audio.avgResponseTime;
+                            valuable.au_avg_res_timex = au_avgResp;
+                        } else {
+                            valuable.au_avg_res_timex = null;
+                        }
+                        if(testData.fineMotor){
+                            const fm_avgTrackTime = testData.fineMotor.avgTrackingTime;
+                            const fm_acc = testData.fineMotor.accuracy;
+                            valuable.fm_avg_trk_time = fm_avgTrackTime;
+                            valuable.fm_accuracy = fm_acc;
+                        } else {
+                            valuable.fm_avg_trk_time = null;
+                            valuable.fm_accuracy = null;
+                        }
+                        if(testData.visual){
+                            let i = 0;
+                            let sum = 0;
+                            for(; testData.visual.responseTimes[i]; i++){
+                                sum += testData.visual.responseTimes[i];
+                            }
+                            const vx_avgResp = sum / i;
+                            const vx_shotAcc = testData.visual.shotAccuracy;
+                            const vx_targetAcc = testData.visual.targetAccuracy;
+                            valuable.vx_avg_res_time = vx_avgResp;
+                            valuable.vx_shot_accuracy = vx_shotAcc;
+                            valuable.vx_trg_accuracy = vx_targetAcc;
+
+    //                      console.log("Visual:\t", vx_avgResp, '\t', vx_shotAcc, '\t', vx_targetAcc);
+                        } else {
+                            valuable.vx_avg_res_time = null;
+                            valuable.vx_shot_accuracy = null;
+                            valuable.vx_trg_accuracy = null;
+                        }
+    //                    console.log(result1.id,  result1.data().timestamp.toDate().toDateString())
+                        returnable.push(valuable);
+                    })
+                })
+            })
+    });
+    console.log("Na weting na")
+    await sleep(5000);
+    console.log("******************", returnable)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+getAllUsersId();
+
+console.log(returnable);
