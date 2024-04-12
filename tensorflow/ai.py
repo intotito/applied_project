9f5cd467aa3e5b9a9f5f56d45adbcbb855373524
intotito <intotito@gmail.com>
2024-04-12 22:11:02 +0100

    ML platform changed to scki-learn from TensorFlow

--- tensorflow/ai.py
+++ tensorflow/ai.py
@@ -6,12 +6,13 @@ import sys
 import json
 import numpy as np
 from dotenv import load_dotenv
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import r2_score
+from sklearn.neural_network import MLPRegressor
 
 # import temporary directory
 import tempfile
@@ -48,7 +49,7 @@ def main(hash):
     plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation='vertical')
     plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
     plt.savefig(tempfile.gettempdir() + "/" + hash + "/correlation_matrix.png")
-
+    plt.close()
     df_independent = df_wanted.iloc[:, -3:]
     df_dependent = df_wanted.iloc[:, :-3]
 
@@ -60,6 +61,22 @@ def main(hash):
     y = scaler.fit_transform(y)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4217)
+
+    # create Neural Network model with 3 input and 6 output
+    model = MLPRegressor(hidden_layer_sizes=(15, 10), activation='tanh', solver='adam', max_iter=1000, verbose=False)
+
+    # train model
+    model.fit(X_train, y_train)
+
+    # plot training and validation loss
+    plt.plot(model.loss_curve_) 
+    plt.title('Training Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(tempfile.gettempdir() + "/" + hash + "/training_loss.png")
+    plt.close()
+
 
     # create Neural Network model with 3 input and 6 output
     #model.add(tf.keras.layers.Dense(3, input_dim=3, activation='tanh'))
@@ -78,7 +95,7 @@ def main(hash):
     #model.save('./ann_model.h5')
 
     #load model
-    model = tf.keras.models.load_model('./ann_model.h5')
+    #model = tf.keras.models.load_model('./ann_model.h5')
 
     # plot training and validation loss
     #plt.plot(model.history.history['loss'])
@@ -90,7 +107,7 @@ def main(hash):
     #plt.close()
 
     # predict
-    y_pred = model.predict(X_test, verbose=0)
+    y_pred = model.predict(X_test)
     #r2_score(y_test, y_pred)
 
     # plot actual vs predicted for each column
@@ -106,7 +123,7 @@ def main(hash):
     plt.close()
 
     # evaluate prediction for all data
-    y_pred_all = model.predict(X, verbose=0)
+    y_pred_all = model.predict(X)
     #r2_score(y, y_pred_all)
 
     # plot actual vs predicted for each column
